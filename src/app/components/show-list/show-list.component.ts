350d0ab4969904e35f0888c8bd3251e0a71727ef
Walndin <valentinzielinski@gmx.de>
2021-10-07 08:51:33 +0200

    2 files modified, 2 added

---
+++ src/app/components/show-list/show-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Show } from '../../model/show';
+
+@Component({
+  selector: 'app-show-list',
+  templateUrl: './show-list.component.html',
+  styleUrls: ['./show-list.component.css']
+})
+export class ShowListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
