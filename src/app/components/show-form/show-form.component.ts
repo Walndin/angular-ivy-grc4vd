e98bd8d1de8ed2473ace1cac7865ac58faf25ffb
Walndin <valentinzielinski@gmx.de>
2021-10-07 11:10:59 +0200

    2 files modified

--- src/app/components/show-form/show-form.component.ts
+++ src/app/components/show-form/show-form.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Show } from '../../model/show';
+import { ShowDataService } from '../../services/show-data.service';
 
 @Component({
   selector: 'app-show-form',
   templateUrl: './show-form.component.html',
-  styleUrls: ['./show-form.component.css']
+  styleUrls: ['./show-form.component.css'],
 })
 export class ShowFormComponent implements OnInit {
+  
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private showDataService: ShowDataService) {
+    class show = Show {
+      return null;
+    }
+    this.show = new Show(null, null);
+    
   }
 
+  ngOnInit() {}
 }
