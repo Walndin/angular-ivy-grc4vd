15a5c42f1029cdbaa0f0187b72cb9c7c3dd9588d
Walndin <valentinzielinski@gmx.de>
2021-10-07 10:15:11 +0200

    3 files modified

--- src/app/services/show-data.service.ts
+++ src/app/services/show-data.service.ts
@@ -7,6 +7,12 @@ export class ShowDataService {
 
   constructor() {
     this.shows.push(new Show(1, 'Paw Patrol'));
-    
+    this.shows.push(new Show(2, 'Prison Break'));
+    this.shows.push(new Show(3, 'Squid Game'));
+    this.shows.push(new Show(4, 'Under The Dome'));
+    this.shows.push(new Show(5, 'The Flash'));
+    this.shows.push(new Show(6, "Dc's Legends of Tomorrow"));
+    this.shows.push(new Show(7, 'Supergirl'));
+    this.shows.push(new Show(8, 'Green Arrow'));
   }
 }
