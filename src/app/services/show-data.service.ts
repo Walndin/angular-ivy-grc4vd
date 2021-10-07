2652aecd1ca27ea3f44e587b77d79d1458bd81fc
Walndin <valentinzielinski@gmx.de>
2021-10-07 10:19:21 +0200

    1 file modified

--- src/app/services/show-data.service.ts
+++ src/app/services/show-data.service.ts
@@ -14,5 +14,7 @@ export class ShowDataService {
     this.shows.push(new Show(6, "Dc's Legends of Tomorrow"));
     this.shows.push(new Show(7, 'Supergirl'));
     this.shows.push(new Show(8, 'Green Arrow'));
+    this.shows.push(new Show(9, 'The Punisher'));
+    this.shows.push(new Show(10, 'The Walking Dead'));
   }
 }
